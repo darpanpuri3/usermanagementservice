f6ef83054eb8f34caae40046b03e0bfd7b4997c5
Darpan Puri <darpanpuri3@gmail.com>
2021-01-06 14:45:33 +0530

    Database Ready

---
+++ src/main/java/UserManagementService/User.java
@@ -0,0 +1,55 @@
+package UserManagementService;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="darpan")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String username;
+	private String firstname;
+	private String lastname;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(Long id, String username, String firstname, String lastname) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+}
